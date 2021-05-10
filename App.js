69b039ae5c4feaf047b58ad9e7fc1c9b5fd81b12
donkndetphone <dinkopeg@posteo.net>
2021-05-10 23:31:42 +0200

    homescreen added

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, Text, View } from 'react-native';
+import {NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import HomeScreen from './screens/HomeScreen';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        appdata: [],
+    } 
+  }
+
+  render(){
+
+    return(
+
+    <NavigationContainer >
+    <Stack.Navigator initialRouteName="Home" headerMode="screen">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        //initialParams={}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
