4c989b84fc2b02b788d756b8ebc872409ba58a46
donkndetphone <dinkopeg@posteo.net>
2021-05-14 22:42:55 +0200

    sophia cam screen added

--- screens/RoomScanScreen_sophia.jsx
+++ screens/RoomScanScreen_sophia.jsx
@@ -1,55 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import styles from '../stylesheet';
-import {View, TouchableOpacity, Image, Button, ImageBackground,} from 'react-native';
-import SplashScreen from  "react-native-splash-screen";
+import React from "react";
+import {
+    View,
+    TouchableOpacity,
+} from "react-native";
 import { Text, Input } from 'react-native-elements';
-import RNCamera from 'react-native-camera';
+import { Camera } from "expo-camera";
+import styles from "../stylesheet";
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 class RoomScanScreen extends React.Component {
+constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            roomscandata: [],
-        }
+    this.state = {
+        roomscandata: [],
+        orientation:ScreenOrientation.OrientationLock.LANDSCAPE,
     }
-
-    render() {
-        return(
-            <View style={styles.containerScan}>
-                <RNCamera
-                    ref={ref => {
-                        this.camera = ref;
-                    }}
-                    style={styles.preview}
-                    // type={RNCamera.Constants.Type.back}
-
-                />
-
-                <View >
-                    <Text style={{justifyContent: 'center',alignSelf: 'center',fontSize: 14, color:'white', }}> Capture your Room </Text>
-                    <TouchableOpacity onPress={ this.onPress()} style={styles.capture}>
-                        <Text style={{ fontSize: 14, color: 'white', }}> SNAP </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        );
-    }
-
-    onPress = async () => {
-        this.takePicture.bind(this);
-        this.props.navigation.navigate('RoomScanConfirmScreen');
-    }
-
-    // takePicture = async () => {
-    //     if (this.camera) {
-    //         this.setState({
-    //             roomscandata: await this.camera.takePictureAsync(options),
-    //         });
-    //     }
-    // }
 }
 
+componentDidMount(){
+    // Start counting when the page is loaded
+    this.timeoutHandle = setTimeout(()=>{
+        this.props.navigation.navigate('RoomScanConfirm')
+    }, 5000);
+}
+
+componentWillUnmount(){
+    clearTimeout(this.timeoutHandle); // This is just necessary in the case that the screen is closed before the timeout fires, otherwise it would cause a memory leak that would trigger the transition regardless, breaking the user experience.
+}
+
+render() {
+    return(
+        <View style={styles.containerScan}>
+            <Camera
+                ref={ref => {
+                    this.camera = ref;
+                }}
+                style={styles.preview}
+                type={Camera.Constants.Type.back}
+            />
+            <View >
+                <Text style={{justifyContent: 'center',alignSelf: 'center',fontSize: 14, color:'white', }}> Capture your Room </Text>
+                <TouchableOpacity onPress={ this.takePicture.bind(this)} style={styles.capture}>
+                    <Text style={{ fontSize: 14, color: 'white', }}> SNAP </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+takePicture = async () => {
+    if (this.camera) {
+        this.setState({
+            roomscandata: await this.camera.takePictureAsync(options),
+        });
+    }
+}
+}
 export default RoomScanScreen;
