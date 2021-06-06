a8933c81069b165676ca3f49e9b5ff49bdc71710
dominikpegler <dinkopeg@posteo.net>
2021-06-06 17:55:29 +0200

    clean up code

--- screens/RoomScanScreen_soph_dmk.jsx
+++ screens/RoomScanScreen_soph_dmk.jsx
@@ -17,29 +17,6 @@ class RoomScanScreen extends React.Component {
     }
   }
   
-  // const [hasPermission, setHasPermission] = useState(null);
-  // const [type, setType] = useState(Camera.Constants.Type.back);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const { status } = await Camera.requestPermissionsAsync();
-  //     setHasPermission(status === 'granted');
-  //   })();
-  // }, []);
-
-  // if (hasPermission === null) {
-  //   return <View />;
-  // }
-  // if (hasPermission === false) {
-  //   return <Text>No access to camera</Text>;
-  // }
-
-  // setTimeout(()=>{
-  //     props.navigation.navigate('RoomScanConfirm')
-  // }, 20000);
-   
-  //return (
-
   componentDidMount(){
        // Start counting when the page is loaded
        this.timeoutHandle = setTimeout(()=>{
